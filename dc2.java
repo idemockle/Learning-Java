1f367feccd1d4a8fb49ba2cddb6372313b33c61c
Ian Kent <iank1226@yahoo.com>
2017-01-06 19:45:42 -0500

    updates

---
+++ dc2.java
@@ -0,0 +1,24 @@
+import java.nio.file.*;
+import java.nio.charset.*;
+import java.io.*;
+
+
+public class dc2 {
+    public static void main(String[] args) {
+        BufferedReader reader;
+        String line;
+        
+        try {
+            reader = Files.newBufferedReader(Paths.get("nums"), StandardCharsets.UTF_8);
+            
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            
+            reader.close();
+        }
+        catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+    }
+}
---
+++ pathtest.java
@@ -0,0 +1,7 @@
+import java.nio.file.*;
+
+public class pathtest {
+    public static void main (String[] args) {
+        System.out.println(Paths.get("green","bleh","wee","nums"));
+    }
+}
